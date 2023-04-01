28ef8d41244c66505ad911f6d37bf8facd87e0f6
Kevin Cárdenas Torres <kevintorres2001sg@gmail.com>
2023-03-31 21:52:37 -0500

    Hoja de vida

---
+++ configuracion.js
@@ -0,0 +1,28 @@
+<table class="styled-table">
+       <h1>Hoja de vida</h1>
+        <thead>
+                <th><h1>Perfil</h1></th>
+                <th><h1>Datos de contacto</h1></th>
+          <tbody>
+                <td><img id="foto" src="foto.jpg" alt=""></td>
+                <td>
+                    <p>Nombre: Kevin Alfonso Cardenas Torres</p> 
+                    <p>Cedula: 1001065743</p> 
+                    <p>Celular: 3508927860</p>
+                    <p>Correo: kevintorres2001sg@gmail.com</p>
+                </td>
+            </tbody>
+        </thead>
+       <thead>
+        <td>
+            <h1>Educación</h1>
+            <p>Bachiller academico</p>
+            <p1>Colegio juan luis londoño la salle</p1>
+            <p>programador backend</p>
+            <p1>Cursando (Universidad distrital)</p1>
+        </td>
+            <td>
+                <h1>Objetivo</h1>
+                <p>Estudiante, con conocimientos en desarrollo de software, bases de datos y lenguajes de programación. mi objetivo principal es crecer profesionalmente, por ende, busco oportunidades que me permitan aprender</p></td>
+       </thead> 
+    </table>
